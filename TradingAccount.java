6f7d019b0cb327da26b5a0d8d46ae92c06bb9d20
Howard Moon <44125785+howardmn@users.noreply.github.com>
2019-06-08 21:19:01 -0700

    Delete TradingAccount.java

--- TradingAccount.java
+++
@@ -1,5 +0,0 @@
-// Specific account type that holds trading account balance and current stocks held.
-public class TradingAccount extends Account {
-    float balance;
-    StockPosition[] stockPositions;
-}
