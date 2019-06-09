bfe9e93a6706b869c3d82e45e9c44a97cb3eae63
Howard Moon <44125785+howardmn@users.noreply.github.com>
2019-06-08 21:37:59 -0700

    Add files via upload

---
+++ src/TradingAccount.java
@@ -0,0 +1,5 @@
+// Specific account type that holds trading account balance and current stocks held.
+public class TradingAccount extends Account {
+    float balance;
+    StockPosition[] stockPositions;
+}
